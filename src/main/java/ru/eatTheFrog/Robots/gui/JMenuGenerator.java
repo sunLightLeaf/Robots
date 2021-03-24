14abc8e48067c00659e6be61e16bf13b70d58764
sunLightLeaf <inhelsmith@gmail.com>
2021-03-24 22:19:59 +0500

    menu generator improved

--- src/main/java/ru/eatTheFrog/Robots/gui/JMenuGenerator.java
+++ src/main/java/ru/eatTheFrog/Robots/gui/JMenuGenerator.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.event.ActionListener;
 
 public class JMenuGenerator {
-    public JMenuBar getJMenuBar(JMenu... menus) {
+    public static JMenuBar getJMenuBar(JMenu... menus) {
         JMenuBar bar = new JMenuBar();
         for (JMenu m : menus)
             bar.add(m);
@@ -12,7 +12,7 @@ public class JMenuGenerator {
     }
 
 
-    private JMenuItem m_getMenuItem(String name, int keyMnemonic, String contextDescription, ActionListener actionListener) {
+    static private JMenuItem m_getMenuItem(String name, int keyMnemonic, String contextDescription, ActionListener actionListener) {
         JMenuItem item = (keyMnemonic == -1) ? new JMenuItem(name) : new JMenuItem(name, keyMnemonic);
         if (contextDescription != null)
             item.getAccessibleContext().setAccessibleDescription(contextDescription);
@@ -20,19 +20,22 @@ public class JMenuGenerator {
         return item;
     }
 
-    private JMenu m_getMenu(String name, int keyMnemonic, String contextDescription, JMenuItem... items) {
-        JMenu menu = (JMenu) m_getMenuItem(name, keyMnemonic, contextDescription, (event) -> {
-        });
+    static private JMenu m_getMenu(String name, int keyMnemonic, String contextDescription, JMenuItem... items) {
+        JMenu menu = new JMenu(name);
+        if (keyMnemonic != -1)
+            menu.setMnemonic(keyMnemonic);
+        if (contextDescription != null)
+            menu.getAccessibleContext().setAccessibleDescription(contextDescription);
         for (JMenuItem i : items)
             menu.add(i);
         return menu;
     }
 
-    public JMenuItem getMenuItem(String name, ActionListener actionListener) {
+    static public JMenuItem getMenuItem(String name, ActionListener actionListener) {
         return m_getMenuItem(name, -1, null, actionListener);
     }
 
-    public JMenu getJMenu(String name, JMenuItem... items) {
+    static public JMenu getJMenu(String name, JMenuItem... items) {
         return m_getMenu(name, -1, null, items);
     }
 
@@ -40,24 +43,24 @@ public class JMenuGenerator {
         return m_getMenuItem(name, -1, contextDescription, actionListener);
     }
 
-    public JMenu getJMenu(String name, String contextDescription, JMenuItem... items) {
+    static public JMenu getJMenu(String name, String contextDescription, JMenuItem... items) {
         return m_getMenu(name, -1, contextDescription, items);
 
     }
 
-    public JMenuItem getMenuItem(String name, int keyMnemonic, ActionListener actionListener) {
+    static public JMenuItem getMenuItem(String name, int keyMnemonic, ActionListener actionListener) {
         return m_getMenuItem(name, keyMnemonic, null, actionListener);
     }
 
-    public JMenu getJMenu(String name, int keyMnemonic, JMenuItem... items) {
+    static public JMenu getJMenu(String name, int keyMnemonic, JMenuItem... items) {
         return m_getMenu(name, keyMnemonic, null, items);
     }
 
-    public JMenuItem getMenuItem(String name, int keyMnemonic, String contextDescription, ActionListener actionListener) {
+    static public JMenuItem getMenuItem(String name, int keyMnemonic, String contextDescription, ActionListener actionListener) {
         return m_getMenuItem(name, keyMnemonic, contextDescription, actionListener);
     }
 
-    public JMenu getJMenu(String name, int keyMnemonic, String contextDescription, JMenuItem... items) {
+    static public JMenu getJMenu(String name, int keyMnemonic, String contextDescription, JMenuItem... items) {
         return m_getMenu(name, keyMnemonic, contextDescription, items);
 
     }
--- src/main/java/ru/eatTheFrog/Robots/gui/MainApplicationFrame.java
+++ src/main/java/ru/eatTheFrog/Robots/gui/MainApplicationFrame.java
@@ -1,20 +1,10 @@
 package ru.eatTheFrog.Robots.gui;
 
-import java.awt.Dimension;
-import java.awt.Toolkit;
-import java.awt.event.KeyEvent;
-
-import javax.swing.JDesktopPane;
-import javax.swing.JFrame;
-import javax.swing.JInternalFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
-
 import ru.eatTheFrog.Robots.log.Logger;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
 
 /**
  * Что требуется сделать:
@@ -43,7 +33,21 @@ public class MainApplicationFrame extends JFrame {
         gameWindow.setSize(400, 400);
         addWindow(gameWindow);
 
-        setJMenuBar(generateMenuBar());
+        setJMenuBar(JMenuGenerator.getJMenuBar(
+                JMenuGenerator.getJMenu(
+                        "Режим отображения", KeyEvent.VK_V, "Управление режимом отображения приложения",
+                        JMenuGenerator.getMenuItem("Системная схема", KeyEvent.VK_S, (event) -> {
+                            setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                            this.invalidate();
+                        }),
+                        JMenuGenerator.getMenuItem("Универсальная схема", KeyEvent.VK_S, (event) -> {
+                            setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+                            this.invalidate();
+                        })
+                ), JMenuGenerator.getJMenu("Тесты", KeyEvent.VK_T, "Тестовые команды",
+                        JMenuGenerator.getMenuItem("Сообщение в лог", KeyEvent.VK_S, (event) -> {
+                            Logger.debug("Новая строка");
+                        }))));
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
@@ -90,49 +94,6 @@ public class MainApplicationFrame extends JFrame {
 //
 //        return menuBar;
 //    }
-
-    private JMenuBar generateMenuBar() {
-        JMenuBar menuBar = new JMenuBar();
-
-        JMenu lookAndFeelMenu = new JMenu("Режим отображения");
-        lookAndFeelMenu.setMnemonic(KeyEvent.VK_V);
-        lookAndFeelMenu.getAccessibleContext().setAccessibleDescription(
-                "Управление режимом отображения приложения");
-
-        {
-            JMenuItem systemLookAndFeel = new JMenuItem("Системная схема", KeyEvent.VK_S);
-            systemLookAndFeel.addActionListener((event) -> {
-                setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-                this.invalidate();
-            });
-            lookAndFeelMenu.add(systemLookAndFeel);
-        }
-
-        {
-            JMenuItem crossplatformLookAndFeel = new JMenuItem("Универсальная схема", KeyEvent.VK_S);
-            crossplatformLookAndFeel.addActionListener((event) -> {
-                setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
-                this.invalidate();
-            });
-            lookAndFeelMenu.add(crossplatformLookAndFeel);
-        }
-
-        JMenu testMenu = new JMenu("Тесты");
-        testMenu.setMnemonic(KeyEvent.VK_T);
-        testMenu.getAccessibleContext().setAccessibleDescription("Тестовые команды");
-
-        {
-            JMenuItem addLogMessageItem = new JMenuItem("Сообщение в лог", KeyEvent.VK_S);
-            addLogMessageItem.addActionListener((event) -> {
-                Logger.debug("Новая строка");
-            });
-            testMenu.add(addLogMessageItem);
-        }
-
-        menuBar.add(lookAndFeelMenu);
-        menuBar.add(testMenu);
-        return menuBar;
-    }
 
     private void setLookAndFeel(String className) {
         try {
